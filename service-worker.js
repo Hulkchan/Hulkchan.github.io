6522b08a0033504aabcc1e47ac5084f79af1baea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-07 08:18:46 +0000

    Deploy Hulkchan/hulkchan.github.io to github.com/Hulkchan/hulkchan.github.io.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -27,23 +27,23 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "2020/11/11/mac显示隐藏文件/index.html",
-    "revision": "39c3830b8152159c947ec342e508d2c8"
+    "revision": "17d194f47a806e9b0e5625c4d2cbc0ce"
   },
   {
     "url": "2020/12/23/js基础知识/index.html",
-    "revision": "8dd5ee7bedb7f17beb010dc945322db7"
+    "revision": "84a8bcf44fa54ff819cd086a19b213d4"
   },
   {
     "url": "404.html",
-    "revision": "d099d94660afec86b40baee61a3b2a52"
+    "revision": "0830dfb35ed4953d26917f9f139ab059"
   },
   {
     "url": "about/index.html",
-    "revision": "0e119c6b61583f5dfd45d11abf672fce"
+    "revision": "59b934752840e6a5c2990bc78f4dd907"
   },
   {
-    "url": "assets/css/0.styles.59b2ca21.css",
-    "revision": "81fdfb18acb0d71254f28f54619ba398"
+    "url": "assets/css/0.styles.afaab5d0.css",
+    "revision": "16da7cfd544a1fd9b8bc414872948303"
   },
   {
     "url": "assets/js/10.d3aacfd6.js",
@@ -54,8 +54,8 @@ self.__precacheManifest = [
     "revision": "2942bdc0fd383f5ef8d1af7ae0ffe39a"
   },
   {
-    "url": "assets/js/2.b1ff16ef.js",
-    "revision": "d00a2e4c19943448953d45c5cd31d958"
+    "url": "assets/js/2.ff584f36.js",
+    "revision": "70e053995ddf38676c2b921971a32ff8"
   },
   {
     "url": "assets/js/3.b4f7b4ca.js",
@@ -74,8 +74,8 @@ self.__precacheManifest = [
     "revision": "2daba34d9dc7f10a6b52b897cf2598f3"
   },
   {
-    "url": "assets/js/7.e61e33aa.js",
-    "revision": "314b919b28c164524a30a2ad566e5c3b"
+    "url": "assets/js/7.0420af9e.js",
+    "revision": "f5f248a6bc71b361792e917ef2e9fee1"
   },
   {
     "url": "assets/js/8.ac3ed5ae.js",
@@ -86,40 +86,40 @@ self.__precacheManifest = [
     "revision": "0358b1583a71ad8325ec76cb600a2281"
   },
   {
-    "url": "assets/js/app.7d214a2a.js",
-    "revision": "31f6e2e4260c987c2d2c7e4a12a12fc5"
+    "url": "assets/js/app.ff94690f.js",
+    "revision": "17af0a00722078cd2e53709f228bfaac"
   },
   {
     "url": "dataCategories/2020-11/index.html",
-    "revision": "7d13823ab3f13e88e86a14eed23dacf9"
+    "revision": "227211549c1b15bc0f8c17c208c7dd96"
   },
   {
     "url": "dataCategories/2020-12/index.html",
-    "revision": "a70170ebada46b4456a94c47d555ee61"
+    "revision": "7223959a92bf1f4dc9be1df366382821"
   },
   {
     "url": "dataCategories/index.html",
-    "revision": "14cf50d465888dd1abc0cf7fdde8e4f1"
+    "revision": "14ccd265a137a5f5e28b803762553257"
   },
   {
     "url": "index.html",
-    "revision": "8d57584042f233f477dcbad81ec6be1f"
+    "revision": "4c34dacdc01fbfd520977b7cb2b9cc9d"
   },
   {
     "url": "tags/index.html",
-    "revision": "3ed309a4b5363690c89a69ade3fd57c7"
+    "revision": "cf717316f9f78be0521a1a3e9c92d4c4"
   },
   {
     "url": "tags/JavaScript/index.html",
-    "revision": "df33bed11576831276102f8425ea36e0"
+    "revision": "02313b847d60fbe8db0a4c45b69bc386"
   },
   {
     "url": "tags/Mac/index.html",
-    "revision": "c7b213e03463cc7160349f2b4f91a858"
+    "revision": "a4a9ceedbc06fc08609822d862ff3d27"
   },
   {
     "url": "tags/前端/index.html",
-    "revision": "c5e1f7a7fd20cc14bfaf3d80f9912f48"
+    "revision": "e0a0ae0fca16e9a4ce510ba23e6311b4"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
