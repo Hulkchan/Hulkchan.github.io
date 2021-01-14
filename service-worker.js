2c55fcd4c733283a609eb74517ff142300399a42
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-14 10:45:03 +0000

    Deploy Hulkchan/Hulkchan.github.io to github.com/Hulkchan/Hulkchan.github.io.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -27,27 +27,27 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "2020/11/11/mac显示隐藏文件/index.html",
-    "revision": "eda1298a8c04ad354d7ad8184766825d"
+    "revision": "bd3476a7df92b4a226e8dae7095e7581"
   },
   {
     "url": "2020/12/23/js基础知识/index.html",
-    "revision": "ad98416c8a1b6fad1f2ef766096f739f"
+    "revision": "a36e1cc4fc5d67bc8f4495dad20e8c9e"
   },
   {
     "url": "2021/01/12/how-to-add-ueditorbtns-in-vue/index.html",
-    "revision": "9b53ceb0134e7305366f88278d45aa39"
+    "revision": "6671c9d99971e9d4ab1f637642f0cf50"
   },
   {
     "url": "404.html",
-    "revision": "c47e659c935b1472a6837bde85469350"
+    "revision": "69ddfe953a7b9de3f42bc249dad71b78"
   },
   {
     "url": "about/index.html",
-    "revision": "0f3509000e045ad2463c655a6eceb3db"
+    "revision": "05a5320aef6e0434319b441034bc8b1f"
   },
   {
-    "url": "assets/css/0.styles.173cba2f.css",
-    "revision": "7a5c5be4d23c53b8e3cc7231869a30ea"
+    "url": "assets/css/0.styles.c5f2f5d8.css",
+    "revision": "f2d41f2689ff18e889a2368e952c7cae"
   },
   {
     "url": "assets/js/10.8b51953d.js",
@@ -70,24 +70,24 @@ self.__precacheManifest = [
     "revision": "aa304aac53fc5ca8e474bd2124882b03"
   },
   {
-    "url": "assets/js/2.0bdaf707.js",
-    "revision": "22091303380272443248299d0230bf9c"
+    "url": "assets/js/2.233c51b2.js",
+    "revision": "7eed2db64f130e5588d05bb04573c591"
   },
   {
-    "url": "assets/js/3.2be6a187.js",
-    "revision": "1d8cc414d68de5cc3435f1dd3a277fd1"
+    "url": "assets/js/3.22a208cc.js",
+    "revision": "70a3fc5c51be6519dcd51759854e3978"
   },
   {
-    "url": "assets/js/4.9caf7a95.js",
-    "revision": "9f04c26df0c6ef76c91099f72bfb9fa0"
+    "url": "assets/js/4.0901d202.js",
+    "revision": "c2b3f90a46666438318f02f6ede2bb6e"
   },
   {
-    "url": "assets/js/5.b1a97fc0.js",
-    "revision": "8a7be914e47d23cf608af69242e26e50"
+    "url": "assets/js/5.7964330a.js",
+    "revision": "1969a6c54e0ebe07b1c9cb65bd48340b"
   },
   {
-    "url": "assets/js/6.232dc695.js",
-    "revision": "11797511d2443d87d4bdfc64fc52568a"
+    "url": "assets/js/6.eb1d1bfb.js",
+    "revision": "58ddd04338b3e4502d05f7803f6c8292"
   },
   {
     "url": "assets/js/7.9bdce64c.js",
@@ -102,48 +102,48 @@ self.__precacheManifest = [
     "revision": "b7d9844dbccc9eb41fcd4f3a93f1ecf7"
   },
   {
-    "url": "assets/js/app.404536d2.js",
-    "revision": "f6edd8a90fc48cfb9580e95486201396"
+    "url": "assets/js/app.a9d8413f.js",
+    "revision": "b0dcc6203910349349c28d2a78086057"
   },
   {
     "url": "dataCategories/2020-11/index.html",
-    "revision": "ddc3cde9d09a756b8f8c54f2ded5cac2"
+    "revision": "f9e00e05dcc23fc1ba05180483b8832a"
   },
   {
     "url": "dataCategories/2020-12/index.html",
-    "revision": "cd210550485dae40c4fcdfa6bea8cb94"
+    "revision": "4a22e2d90097f10dd71e007456aed2a3"
   },
   {
     "url": "dataCategories/2021-01/index.html",
-    "revision": "38c99a87f776dd0521ef3a930551e929"
+    "revision": "0f8bab3a0cf55cb04dfb597840aee66e"
   },
   {
     "url": "dataCategories/index.html",
-    "revision": "491a3872c2a001ef8e7a61752784bab8"
+    "revision": "56d788ff9aee874f6c50ab678ba94954"
   },
   {
     "url": "index.html",
-    "revision": "33d416841e5df95ef4c538b10e4c6beb"
+    "revision": "22ca53520c4c18829fb3d8d43505b41b"
   },
   {
     "url": "tags/index.html",
-    "revision": "c29f0803ca514e5c4bb066a8756f4ef0"
+    "revision": "390d6606eaedf9455b58081109298792"
   },
   {
     "url": "tags/JavaScript/index.html",
-    "revision": "7c4a606b8b4ed4816d267ee6fe28d220"
+    "revision": "20c1a0009f621586364e0b89fb18631d"
   },
   {
     "url": "tags/Mac/index.html",
-    "revision": "10e8f185fe1fb052bcdcf8780a61fe3d"
+    "revision": "8195056574155acd2903dd297e5f6482"
   },
   {
     "url": "tags/前端/index.html",
-    "revision": "146935228722b4a9342b24ac57f4b526"
+    "revision": "351a27c23a96db599c4adcb185374ba9"
   },
   {
     "url": "tags/工作小问题/index.html",
-    "revision": "b1c731af43383d75ac4712883576cf69"
+    "revision": "826157f8240406b2845b59e4d5cb8d51"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
